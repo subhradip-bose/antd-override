778710f1fdeb982837fde7fc3a4cf4c3b1c2a76b
Subhradip Bose <subhradip.bose@Subhradips-MacBook-Air.local>
2024-03-13 16:08:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export * from "antd/lib";
 export * from "antd/lib/layout";
 export * from "antd/lib/layout/layout";
 export * from "antd/lib/modal/Modal";
+export { Text } from "antd/lib/typography/Text";
